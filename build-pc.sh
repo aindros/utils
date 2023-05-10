ab33ee4ff2116646d796f6da5fa0950a8695d20e
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-05-10 12:48:26 +0200

    Add variables anch some check in build-pc.sh

--- build-pc.sh
+++ build-pc.sh
@@ -1,15 +1,52 @@
 #!/bin/sh
 
-cat <<EOF > print.sh
-prefix=$1
+VERSION=0.0.1
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+		-p|--prefix)
+			shift
+			PREFIX=$1
+		;;
+		-n|--name)
+			shift
+			NAME=$1
+		;;
+		-ln|--lib-name)
+			shift
+			LIBNAME=$1
+		;;
+		-d|--description)
+			shift
+			DESCR=$1
+		;;
+		-v|--version)
+			shift
+			VERSION=$1
+		;;
+	esac
+	shift
+done
+
+if [ -z $PREFIX ]; then
+	echo 'prefix must be set'
+	exit 1
+elif [ -z $NAME ]; then
+	echo 'name must be set'
+	exit 1
+fi
+
+cat <<EOF > utils.pc
+prefix=$PREFIX
 exec_prefix=\${prefix}
 includedir=\${prefix}/include
 libdir=\${exec_prefix}/lib
 
 
-Name: $2
-Description: $3 - $4
-Version: $5
+Name: $NAME
+Description: $LIBNAME - $DESCR
+Version: $VERSION
 Cflags: -I\${includedir}
-Libs: -L\${libdir} -l$2
+Libs: -L\${libdir} -l$NAME
 EOF
